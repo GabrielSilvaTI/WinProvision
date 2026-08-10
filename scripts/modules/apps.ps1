b69b0e67d391ba011b8be17e186c43bb91893432
GabrielSilvaTI <gabriel.silva20090@gmail.com>
2026-08-09 22:17:22 -0300

    Criação do módulo apps

---
+++ scripts/modules/apps.ps1
@@ -0,0 +1,5 @@
+winget install --id=Valve.Steam -e --silent ;
+winget install --id=HydraLauncher.Hydra -e --silent ;
+winget install --id=Google.Chrome -e --silent ;
+winget install --id=LocalSend.LocalSend -e --silent ;
+winget install --id=RARLab.WinRAR -e --silent
