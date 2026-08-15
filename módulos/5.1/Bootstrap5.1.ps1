e96e425a6ea70162d2dfa1a4029c9b3c4f8a4218
GabrielSilvaTI <gabriel.silva20090@gmail.com>
2026-08-15 15:32:16 -0300

    Add files via upload

---
+++ módulos/5.1/Bootstrap5.1.ps1
@@ -0,0 +1,9 @@
+﻿# Roda no Windows PowerShell 5.1 (Administrador)
+Set-Location -Path "C:\Users\Gabriel\Downloads\Testes"
+
+# Importa e instala o PS7
+Import-Module .\Install-PowerShell7.psd1 -Force
+Install-PowerShell7
+
+# Uma vez instalado, chama o PowerShell 7 para rodar o restante dos seus módulos!
+pwsh.exe -NoProfile -Command "& { Set-Location 'C:\Users\Gabriel\Downloads\Testes'; Get-ChildItem -Filter *.psd1 | Import-Module -Force; Install-Winget; Install-Office; Invoke-WinProvision }"
